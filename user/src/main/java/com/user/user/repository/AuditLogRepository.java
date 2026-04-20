9441a676ea40dccf8306e4ef886aee8878f420f1
Rohit Pawar <80481126+rohitpawar2000@users.noreply.github.com>
2026-04-20 18:36:32 +0530

    Delete user directory

--- user/src/main/java/com/user/user/repository/AuditLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.user.user.repository;
-
-import com.user.user.entity.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long >{
-
-}
--- user/src/main/java/com/user/user/repository/FundRepository.java
+++
@@ -1,8 +0,0 @@
-package com.user.user.repository;
-
-import com.user.user.entity.Fund;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FundRepository extends JpaRepository<Fund,Long> {
-
-}
